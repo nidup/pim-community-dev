5ff754b8d8d9eec283213b8c909acd1a6a3bc199
Antoine Guigan <antoine@akeneo.com>
2014-06-18 11:30:23 +0200

    Added spec for IteratorReader

---
+++ spec/Pim/Bundle/BaseConnectorBundle/Reader/AbstractIteratorReaderSpec.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace spec\Pim\Bundle\BaseConnectorBundle\Reader;
+
+use Akeneo\Bundle\BatchBundle\Entity\StepExecution;
+use PhpSpec\ObjectBehavior;
+use Pim\Bundle\BaseConnectorBundle\Reader\AbstractIteratorReader;
+
+class AbstractIteratorReaderSpec extends ObjectBehavior
+{
+    public function let()
+    {
+        $this->beAnInstanceOf('spec\Pim\Bundle\BaseConnectorBundle\Reader\StubArrayIteratorReader');
+    }
+
+    public function it_is_initializable()
+    {
+        $this->beConstructedWith(array());
+        $this->shouldHaveType('spec\Pim\Bundle\BaseConnectorBundle\Reader\StubArrayIteratorReader');
+    }
+
+    public function it_iterates_through_values()
+    {
+        $values = array('value1', 'value2', 'value3');
+        $this->beConstructedWith($values);
+        foreach ($values as $value) {
+            $this->read()->shouldReturn($value);
+        }
+        $this->read()->shouldReturn(null);
+    }
+
+    public function it_sends_all_values_in_bulk_mode()
+    {
+        $values = array('value1', 'value2', 'value3');
+        $this->beConstructedWith($values, true);
+        $this->read()->shouldReturn($values);
+        $this->read()->shouldReturn(null);
+    }
+
+    public function it_increments_the_summary_info(StepExecution $stepExecution)
+    {
+        $values = array('value1', 'value2', 'value3');
+        $this->beConstructedWith($values);
+        $this->setStepExecution($stepExecution);
+        $stepExecution->incrementSummaryInfo('read')->shouldBeCalledTimes(count($values));
+        for ($i=0; $i < count($values); $i++) {
+            $this->read();
+        }
+    }
+
+    public function it_can_be_resetted()
+    {
+        $values = array('value1', 'value2', 'value3');
+        $this->beConstructedWith($values);
+        foreach ($values as $value) {
+            $this->read()->shouldReturn($value);
+        }
+        $this->reset();
+        foreach ($values as $value) {
+            $this->read()->shouldReturn($value);
+        }
+    }
+}
+
+class StubArrayIteratorReader extends AbstractIteratorReader
+{
+    /**
+     * @var array
+     */
+    protected $values;
+
+    public function __construct(array $values, $batchMode = false)
+    {
+        $this->values = $values;
+        parent::__construct($batchMode);
+    }
+
+    protected function createIterator()
+    {
+        return new \ArrayIterator($this->values);
+    }
+
+    public function getConfigurationFields()
+    {
+        return array();
+    }
+}
