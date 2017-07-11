591bb8cbeaf689123651f4eb3a5c8cac6f4d1a16
Nicolas Dupont <nicolas@akeneo.com>
2017-07-11 10:07:20 +0200

    Update js

--- src/Pim/Bundle/UIBundle/Resources/public/js/app.js
+++ src/Pim/Bundle/UIBundle/Resources/public/js/app.js
@@ -3,7 +3,7 @@ define(['jquery', 'underscore'],
         'use strict';
 
         /**
-         * Main Application
+         * Main Applicationz
          *
          * @export oro/app
          * @name oro.app
